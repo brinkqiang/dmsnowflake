4d5d6cea932761673ae7aaffd54a8db6001e8e64
brinkqiang <brink.qiang@gmail.com>
2020-04-07 16:09:11 +0800

    add

--- include/dmsnowflake.h
+++ include/dmsnowflake.h
@@ -25,18 +25,18 @@
 #include <stdint.h>
 #include <mutex>
 
- // �ṹΪ��
+ // 结构为：
  //
  // 0---0000000000 0000000000 0000000000 0000000000 0 --- 00000 ---00000 ---0000000000 00
- // ��������ַ����У���һλΪδʹ�ã�ʵ����Ҳ����Ϊlong�ķ���λ������������41λΪ���뼶ʱ�䣬Ȼ��5λdatacenter��ʶλ��
- // 5λ����ID���������ʶ����ʵ����Ϊ�̱߳�ʶ����Ȼ��12λ�ú����ڵĵ�ǰ�����ڵļ������������պ�64λ��Ϊһ��Long�͡�
+ // 在上面的字符串中，第一位为未使用（实际上也可作为long的符号位），接下来的41位为毫秒级时间，然后5位datacenter标识位，
+ // 5位机器ID（并不算标识符，实际是为线程标识），然后12位该毫秒内的当前毫秒内的计数，加起来刚好64位，为一个Long型。
  //
- // �����ĺô��ǣ������ϰ���ʱ���������򣬲��������ֲ�ʽϵͳ�ڲ������ID��ײ����datacenter�ͻ���ID�����֣���
- // ����Ч�ʽϸߣ������ԣ�snowflakeÿ���ܹ�����26��ID���ң���ȫ������Ҫ��
+ // 这样的好处是，整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞（由datacenter和机器ID作区分），
+ // 并且效率较高，经测试，snowflake每秒能够产生26万ID左右，完全满足需要。
  //
+
 class IdWorkerUnThreadSafe {
 public:
-    // TODO(@benqi): ���worker_id��data_center_id
     IdWorkerUnThreadSafe(uint16_t worker_id, uint16_t data_center_id)
         : worker_id_(worker_id),
         data_center_id_(data_center_id) {}

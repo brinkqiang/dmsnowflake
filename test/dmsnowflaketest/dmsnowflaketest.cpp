107be54fb8e393618da3f23f3c08b3a03b1c55a9
brinkqiang <brink.qiang@gmail.com>
2020-04-07 17:33:34 +0800

    add

--- test/dmsnowflaketest/dmsnowflaketest.cpp
+++ test/dmsnowflaketest/dmsnowflaketest.cpp
@@ -3,17 +3,21 @@
 
 #include "gtest.h"
 
+#include <unordered_set>
 
 TEST(CDMIDGenerator, CDMIDGenerator) {
     CDMIDGenerator oID;
 
     int c = 1000000;
-
+    std::unordered_set<uint64_t> keys;
     for (int i = 0; i < c; ++i)
     {
-        oID.GetNextID();
+        uint64_t id = oID.GetNextID();
+
+        auto it = keys.find(id);
+        ASSERT_TRUE(it == keys.end());
+        keys.insert(id);
     }
 }
-
 
 

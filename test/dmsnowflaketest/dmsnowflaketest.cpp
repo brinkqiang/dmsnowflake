1ae56d43f0b5d7679310f529db1af3caab721a77
brinkqiang <brink.qiang@gmail.com>
2020-10-16 16:21:00 +0800

    add

--- test/dmsnowflaketest/dmsnowflaketest.cpp
+++ test/dmsnowflaketest/dmsnowflaketest.cpp
@@ -21,18 +21,3 @@ TEST(dmsnowflake_h, dmsnowflake_h) {
 		keys.insert(id);
 	}
 }
-
-
-TEST(snowflake_hpp, snowflake_hpp) {
-    id_generator id_gen(0, 0);
-
-    std::unordered_set<uint64_t> keys;
-    for (int i = 0; i < gen_count; ++i)
-    {
-        uint64_t id = id_gen.get_nextid();
-
-        auto it = keys.find(id);
-        ASSERT_TRUE(it == keys.end());
-        keys.insert(id);
-    }
-}

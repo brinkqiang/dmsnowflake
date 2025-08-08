59ce9fb98820107b4480f26e0943e75abb8ff516
brinkqiang <brink.qiang@gmail.com>
2025-08-08 15:41:35 +0800

    add

--- build.bat
+++ build.bat
@@ -1,14 +1,10 @@
 
-rem - clone code
-rem git clone https://github.com/brinkqiang/dmsnowflake.git
-rem pushd dmsnowflake
-rem git submodule update --init --recursive
-
 rmdir /S /Q build
-mkdir build
-pushd build
+mkdir build 2> nul
+cd build
+
 cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo ..
-cmake --build . --config relwithdebinfo
-popd
+cmake --build . --config relwithdebinfo -- /m:%NUMBER_OF_PROCESSORS%
+cd ..
 
 rem pause
--- build.sh
+++ build.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
 
-# - install depends tools
-# yum -y install git
-# yum -y install gcc gcc-c++ autoconf libtool automake make
-#
-
-# - clone code
-# git clone https://github.com/brinkqiang/dmsnowflake.git
-# pushd dmsnowflake
-# git submodule update --init --recursive
-#
-
-# pushd thirdparty/depends_path
-# libtoolize && aclocal && autoheader && autoconf && automake --add-missing
-# sh configure
-# popd
-
 rm -rf build
 mkdir -p build
-pushd build
-cmake -DCMAKE_BUILD_TYPE=relwithdebinfo ..
-cmake --build . --config relwithdebinfo
-popd
+cd build
 
-# popd
+cmake -DCMAKE_BUILD_TYPE=relwithdebinfo ..
+cmake --build . --config relwithdebinfo -- -j$(nproc)
+
+cd ..
 
 # echo continue && read -n 1
